fde4c9198adc53971aff8883c72791f39de925fa
balasankar1 <balasankar0143@gmail.com>
2023-03-16 16:31:45 +0530

    added feature of getting nodeId with query strings in update page sections and thus update js of dashboard

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -7,14 +7,14 @@ window.addEventListener('load',()=>{
 const cardContainer = document.querySelector(".card-container");
 
 const cardData=[
-    {  heading:"heading1",content:"hasfhbwasyufgasdfugsdfyusdfhgsdyufhsdfyugsdfyusdhfiuasdhfsdhfesdjfsdfbhuwsehfyuweafas dfytegfyeasd fytesvfyteas faygsdc std fvhygsdt fsdgf sdfgcdfytc ysdveygstdc faystdcf saytfcasdgfc hstdfvcsdhgfc wedrtgfeyusdfeaiusdgf",id:1},
-    {  heading:"heading1",content:"content",id:1},
-    {  heading:"heading1",content:"content",id:1},
-    {  heading:"heading1",content:"content",id:1},
-    {  heading:"heading1",content:"content",id:1},
-    {  heading:"heading1",content:"content",id:1},
-    {  heading:"heading1",content:"content",id:1},
-    {  heading:"heading1",content:"content",id:1},
+    {  heading:"heading1",content:"hasfhbwasyufgasdfugsdfyusdfhgsdyufhsdfyugsdfyusdhfiuasdhfsdhfesdjfsdfbhuwsehfyuweafas dfytegfyeasd fytesvfyteas faygsdc std fvhygsdt fsdgf sdfgcdfytc ysdveygstdc faystdcf saytfcasdgfc hstdfvcsdhgfc wedrtgfeyusdfeaiusdgf",id:0},
+    {  heading:"heading2",content:"content",id:1},
+    {  heading:"heading3",content:"content",id:2},
+    {  heading:"heading4",content:"content",id:3},
+    {  heading:"heading5",content:"content",id:4},
+    {  heading:"heading6",content:"content",id:5},
+    {  heading:"heading7",content:"content",id:6},
+    {  heading:"heading8",content:"content",id:7},
 ]
 
 
@@ -26,7 +26,9 @@ array.forEach(cardObj =>{
     card.classList.add("card");
     card.id=id;
 
-    const innerhtml = ` <div class="card-header"><div class="card-heading">${heading}</div><!-- <div class="edit-note"> <img src="" alt=""></div> --></div><div class="card-content">${content}</div>`
+    const innerhtml = `  <div class="card-header">
+    <div class="card-heading">${heading}</div><a href="../updatenotes/updatenotes.html?noteId=${id}">
+    <div class="edit-note"> <img src="../../assets/edit-note.svg" alt=""></div> </div><div class="card-content">${content}</div>`
     card.innerHTML = innerhtml;
     cardContainer.appendChild(card);
     
